ba08e1bd0204a397516c5af24b70aace56746dcd
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-11-16 20:42:58 +0100

    test

---
+++ src/test/java/com/example/demo/service/MovieServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+  @Test
+  void getFirstMovie() {
+    String name = "test";
+    assertEquals("test", name);
+  }
+
+  @Test
+  void getRandomMovie() {
+  }
+}
